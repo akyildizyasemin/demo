2feaf3a6ce502e034c28d70640aa08b52f4eabda
Yasemin AKYILDIZ <yaseminarsivi@gmail.com>
2023-05-16 00:16:03 +0300

    .

--- src/main/java/com/example/ECommerceApp/webApi/controllers/CategoryController.java
+++ src/main/java/com/example/ECommerceApp/webApi/controllers/CategoryController.java
@@ -1,8 +1,9 @@
 package com.example.ECommerceApp.webApi.controllers;
+
 import com.example.ECommerceApp.business.abstracts.CategoryService;
 import com.example.ECommerceApp.business.requests.CreateCategoryRequest;
+import com.example.ECommerceApp.business.requests.UpdateCategoryRequest;
 import com.example.ECommerceApp.business.responses.CategoryResponse;
-import com.example.ECommerceApp.entities.concretes.Category;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +22,20 @@ public class CategoryController {
     private CategoryService categoryService;
     private final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
-    @PostMapping("/create")
-    public ResponseEntity<?> createCategory(CreateCategoryRequest createCategoryRequest) {
-        logger.debug("A new category has been created.");
-        this.categoryService.createCategory(createCategoryRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
 
+    @PostMapping("/create")
+    public ResponseEntity<?> createCategory(@RequestBody CreateCategoryRequest createCategoryRequest) {
+        logger.info("Creating a new category...");
+
+        try {
+            categoryService.createCategory(createCategoryRequest);
+            logger.info("Category created successfully");
+            return ResponseEntity.ok("Category created successfully");
+        } catch (Exception e) {
+            logger.error("Failed to create category: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create category");
+        }
+    }
     @GetMapping()
     public ResponseEntity<List<CategoryResponse>> getAllCategories(){
         logger.debug("All categories have been retrieved.");
@@ -35,9 +43,15 @@ public class CategoryController {
         return ResponseEntity.ok(categories);
     }
 
-//    @PostMapping("/update/{categoryId}")
-//    public ResponseEntity<Category> updateCategory(@PathVariable String categoryId, @RequestBody Category category){
-//        return new ResponseEntity<>(categoryService.updateCategory(category, categoryId), HttpStatus.OK);
-//    }
-
+    @PutMapping("/update")
+    public ResponseEntity<String> updateCategory(@RequestBody UpdateCategoryRequest updateCategoryRequest) {
+        try {
+            categoryService.updateCategory(updateCategoryRequest);
+            logger.info("Category updated successfully.");
+            return ResponseEntity.ok("Category updated successfully.");
+        } catch (Exception e) {
+            logger.error("Failed to update category: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update category.");
+        }
+    }
 }
